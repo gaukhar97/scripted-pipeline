f7a397118125a8a5cb04b41c2a55dc192d80860b
gaukhar97 <imankulova@yahoo.com>
2020-12-10 19:08:44 +0000

    added lines

--- terraform-vpc.groovy
+++ terraform-vpc.groovy
@@ -1,7 +1,8 @@
 properties([
     parameters([
         booleanParam(defaultValue: true, description: 'are you sure you wanna run this command?', name: 'terraform_apply'), 
-        booleanParam(defaultValue: false, description: 'are you sure you wanna run this command?', name: 'terraform_destroy')
+        booleanParam(defaultValue: false, description: 'are you sure you wanna run this command?', name: 'terraform_destroy'),
+        choice(choices: ['dev', 'qa', 'prod'], description: 'Choose environment', name: 'environment')
     ])
 ])
 
@@ -22,21 +23,21 @@ node{
             if(params.terraform_apply){
                 stage("Terraform Apply"){
                     sh """
-                        terraform apply -var-file dev.tfvars -auto-approve
+                        terraform apply -var-file ${param.environment}.tfvars -auto-approve
                     """
                 } 
             }
             else if(params.terraform_destroy){
                 stage("Terraform Destroy"){
                     sh """
-                        terraform destroy -var-file dev.tfvars -auto-approve
+                        terraform destroy -var-file ${param.environment}.tfvars -auto-approve
                     """
                 }
             }
             else {
                 stage("Terraform Plan"){
                     sh """
-                        terraform plan -var-file dev.tfvars
+                        terraform plan -var-file ${param.environment}.tfvars
                     """
                 }   
             }     
