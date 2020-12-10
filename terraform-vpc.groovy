bdf7adbc94f9aa81539bde4079b2ed9923cb4b95
gaukhar97 <imankulova@yahoo.com>
2020-12-10 04:47:39 +0000

    added lines

---
+++ terraform-vpc.groovy
@@ -0,0 +1,45 @@
+properties([
+    parameters([
+        booleanParam(defaultValue: true, description: 'are you sure you wanna run this command?', name: 'terraform_apply'), 
+        booleanParam(defaultValue: false, description: 'are you sure you wanna run this command?', name: 'terraform_destroy')
+    ])
+])
+
+node{
+    stage("Pull Repo"){
+        git branch: 'master', url: 'https://github.com/gaukhar97/terraform-vpc.git'
+    }
+    
+    withEnv(['AWS_REGION=us-east-1']) {
+        withCredentials([usernamePassword(credentialsId: 'aws_jenkins_key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+            stage("Terraform Init"){
+                sh """
+                    bash setenv.sh dev.tfvars
+                    terraform init
+                """
+            }
+
+            if(params.terraform_apply){
+                stage("Terraform Apply"){
+                    sh """
+                        terraform apply -var-file dev.tfvars -auto-approve
+                    """
+                } 
+            }
+            else if(params.terraform_destroy){
+                stage("Terraform Destroy"){
+                    sh """
+                        terraform destroy -var-file dev.tfvars -auto-approve
+                    """
+                }
+            }
+            else {
+                stage("Terraform Plan"){
+                    sh """
+                        terraform plan -var-file dev.tfvars
+                    """
+                }   
+            }     
+        }
+    }      
+} 
--- terraform-worker.groovy
+++ terraform-worker.groovy
@@ -1,17 +1,44 @@
- node{
+properties([
+    parameters([
+        booleanParam(defaultValue: true, description: 'are you sure you wanna run this command?', name: 'terraform_apply'), 
+        booleanParam(defaultValue: false, description: 'are you sure you wanna run this command?', name: 'terraform_destroy')
+    ])
+])
+
+node{
     stage("Pull Repo"){
         git branch: 'solution', url: 'https://github.com/ikambarov/terraform-task.git'
     }
     
     dir('sandbox/') {
-    stage("Terraform Init"){
-        sh """
-            terraform init
-        """
-    }
-    stage("Terraform Apply"){
-        sh """
-            terraform apply
-        """
+        withCredentials([usernamePassword(credentialsId: 'aws_jenkins_key', passwordVariable: 'AWS_ACCESS_SECRET_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+            stage("Terraform Init"){
+                sh """
+                    terraform init
+                """
+            }
+
+        if(params.terraform_apply){
+            stage("Terraform Apply"){
+                sh """
+                    terraform apply  -auto-approve
+                """
+            }
+        }
+        else if(params.terraform_destroy){
+            stage("Terraform Destroy"){
+                sh """
+                    terraform destroy -auto-approve
+                """
+            }
+        }
+        else {
+            stage("Terraform Plan"){
+                sh """
+                    terraform plan
+                """
+                }
+            }   
+        }     
     }
 } 
